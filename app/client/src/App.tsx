9ec0dd7cac1dffafcd23477b88ee94af01227195
TristanGrlt <tristan.groult@gmail.com>
2026-01-17 23:54:07 +0100

    feat(routes): update login route to render Login component instead of LandingPage

--- app/client/src/App.tsx
+++ app/client/src/App.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from 'react-router-dom'
 import { LandingPage } from './routes/landingPage.tsx'
 import { SideBar } from './components/admin/sideBar.tsx'
+import { Login } from './routes/login.tsx'
 
 function App() {
   return (
     <Routes>
       <Route index element={<LandingPage />} />
-      <Route path='login' element={<LandingPage />} />
+      <Route path='login' element={<Login />} />
       <Route path='admin' element={<SideBar />}>
         <Route path='toto' element={<LandingPage />} />
       </Route>
