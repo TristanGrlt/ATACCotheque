6d45e848373447ec83cf74ad050a5b0f82c51d49
TristanGrlt <tristan.groult@gmail.com>
2026-01-18 00:32:15 +0100

    feat(auth): implement user authentication with JWT and cookie handling

--- app/server/controllers/user.controller.ts
+++ app/server/controllers/user.controller.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import User, { IUser } from '../models/user.model.js'
 import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
 
 export const addUser = async (req: Request<{}, {}, IUser>, res: Response) => {
   try {
@@ -29,5 +30,34 @@ export const addUser = async (req: Request<{}, {}, IUser>, res: Response) => {
       res.status(400).json({ error: error.message });
     } 
     res.status(500).json({ error: 'Internal server error' });
+  }
+};
+
+export const connectUser = async (req: Request<{}, {}, IUser>, res: Response) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username });
+  if (!user) {
+    return res.status(401).json({ error: `Nom d'utilisateur ou mot de passe incorrect` });
+  }
+
+  const match = await bcrypt.compare(password, user.password);
+  if (match) {
+    const jwtSecret = process.env.JWT_SECRET
+    if (!jwtSecret) {
+      throw new Error("JWT_SECRET environment variable is not defined");
+    }
+    const jsToken = jwt.sign({ userId: user._id }, jwtSecret);
+    res.cookie('jwt', jsToken, {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+      maxAge: 24 * 60 * 60 * 1000 // 1 day
+    });
+
+    const { password: _pw, ...userData } = user.toObject();
+    return res.status(200).json(userData)
+  } else {
+    return res.status(401).json({ error: `Nom d'utilisateur ou mot de passe incorrect` });
   }
 };
