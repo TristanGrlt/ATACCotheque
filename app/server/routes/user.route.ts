9cf360a1565bc259044287e48561675ddc9ce288
TristanGrlt <tristan.groult@gmail.com>
2026-01-18 01:23:45 +0100

    refactor(routes): update user routes to use /signup instead of /, /login, and /logout endpoints and add token verification for /signup endpoint

--- app/server/routes/user.route.ts
+++ app/server/routes/user.route.ts
@@ -1,8 +1,11 @@
 import { Router, Request, Response } from 'express';
-import { addUser } from '../controllers/user.controller.js';
+import { signupUser, loginUser, logoutUser } from '../controllers/user.controller.js';
+import { verifyToken } from '../middlewares/verifyToken.js';
 
 const router = Router();
 
-router.post('/', addUser);
+router.post('/signup', verifyToken, signupUser);
+router.post('/login', loginUser);
+router.post('/logout', logoutUser);
 
 export default router;
