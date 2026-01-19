6c8e3ed2a9fca075ae8dc6cf7087dc4a035b90b9
TristanGrlt <tristan.groult@gmail.com>
2026-01-19 23:47:29 +0100

    fix: add shadow to Card component in Login for improved UI

--- app/client/src/routes/login.tsx
+++ app/client/src/routes/login.tsx
@@ -56,7 +56,7 @@ export function Login() {
 
   return (
     <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-      <Card className="w-full max-w-sm">
+      <Card className="w-full max-w-sm shadow-xl">
         <CardHeader>
           <div className="flex flex-col items-center gap-2 text-center">
             <a>
