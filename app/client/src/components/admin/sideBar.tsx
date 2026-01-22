ca86f28a4bab905d7bcec8a69436e5db40490f41
TristanGrlt <tristan.groult@gmail.com>
2026-01-22 19:01:00 +0100

    feat: refactor SideBar component to use Sidebar UI elements and add logo with test categorie

--- app/client/src/components/admin/sideBar.tsx
+++ app/client/src/components/admin/sideBar.tsx
@@ -1,12 +1,146 @@
 import { Outlet } from "react-router-dom"
+import { Calendar, Home, Inbox, Search, Settings } from "lucide-react"
+ 
+import {
+  Sidebar,
+  SidebarContent,
+  SidebarFooter,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarHeader,
+  SidebarInset,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
+  SidebarProvider,
+  SidebarTrigger,
+} from "@/components/ui/sidebar"
+
+import logo from '/atacc_logo.png'
+import { Separator } from "../ui/separator"
+import { Button } from "../ui/button"
+
+const items = [
+  {
+    title: "Home",
+    url: "#",
+    icon: Home,
+  },
+  {
+    title: "Inbox",
+    url: "#",
+    icon: Inbox,
+  },
+  {
+    title: "Calendar",
+    url: "#",
+    icon: Calendar,
+  },
+  {
+    title: "Search",
+    url: "#",
+    icon: Search,
+  },
+  {
+    title: "Settings",
+    url: "#",
+    icon: Settings,
+  },
+]
 
 export function SideBar() {
   return (
-    <div className="relative flex min-h-svh flex-col bg-background md:pl-20">
-      side bar
-      <main className="flex flex-1 flex-col px-0 pb-28 pt-6 md:px-8 md:pb-12">
-        <Outlet />
-      </main>
-    </div>
+    <SidebarProvider 
+    style={
+        {
+          "--sidebar-width": "calc(var(--spacing) * 72)",
+          "--header-height": "calc(var(--spacing) * 12)",
+        } as React.CSSProperties
+      }
+    >
+      <Sidebar collapsible="offcanvas">
+        <SidebarHeader>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton
+              asChild
+              className="data-[slot=sidebar-menu-button]:!p-1.5"
+            >
+              <a href="/">
+                <img src={logo} alt="logo" width={25} />
+                <span className="text-base font-semibold">Attacothèque.</span>
+              </a>
+            </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarHeader>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel>Application</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+        <SidebarFooter>
+          <SidebarMenu>
+            <SidebarMenuItem>
+              <SidebarMenuButton size="lg">
+              User
+            </SidebarMenuButton>
+            </SidebarMenuItem>
+          </SidebarMenu>
+        </SidebarFooter>
+      </Sidebar>
+      <SidebarInset >
+        <header className="flex h-(--header-height) shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-(--header-height)">
+          <div className="flex w-full items-center gap-1 px-4 lg:gap-2 lg:px-6">
+            <SidebarTrigger className="-ml-1" />
+            <Separator
+              orientation="vertical"
+              className="mx-2 data-[orientation=vertical]:h-4"
+            />
+            <h1 className="text-base font-medium">Paneau d'administration</h1>
+            <div className="ml-auto flex items-center gap-2">
+              <Button variant="ghost" asChild size="sm" className="hidden sm:flex">
+                <a
+                  href="#"
+                  rel="noopener noreferrer"
+                  target="_blank"
+                  className="dark:text-foreground"
+                >
+                  GitHub
+                </a>
+              </Button>
+            </div>
+          </div>
+        </header>
+        <div className="flex flex-1 flex-col">
+          <div className="@container/main flex flex-1 flex-col gap-2">
+            <Outlet />
+          </div>
+        </div>
+
+
+
+      {/* <main className="bg-[var(--sidebar)]">
+        <SidebarTrigger />
+        <div className="rounded-md m-3 h-full">
+        </div>
+      </main> */}
+      </SidebarInset>
+    </SidebarProvider>
   )
 }
