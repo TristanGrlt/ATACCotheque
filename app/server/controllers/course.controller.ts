b8c6259d9aab06ca03c7672e5574fa24560dd19e
louisgrn <louis.guerain76@gmail.com>
2026-02-05 18:20:11 +0100

    refactor: replace Mongoose queries with Prisma for course retrieval

--- app/server/controllers/course.controller.ts
+++ app/server/controllers/course.controller.ts
@@ -1,32 +1,29 @@
-import Course, { ICourse } from '../models/course.model.js'
-import Level from '../models/level.model.js'
-import Major from '../models/major.model.js'
-import mongoose from 'mongoose'
 import { Request, Response } from 'express'
+import prisma from '../lib/prisma.js';
 
 export const getCourse = async (req: Request, res: Response) => {
 
-    const cycle = req.query.cycle ? String(req.query.cycle) : ''
-    const major = req.query.major ? String(req.query.major) : ''
-
-    const findMajor = await Major.findById(major);
-    if(findMajor == null){
-      return res.json({ courses: [] })
+const result = await prisma.major.findMany({
+ 
+ 
+  select: {
+    name: true, 
+    level: {
+      select: {
+        name: true, 
+        course: {
+          select: {
+            id : true,
+            name: true 
+           
+          }
+        }
+      }
     }
+  }
+});
 
-    const idMajor =  findMajor._id;
-
-    const findLevel = await Level.findOne({ name: cycle, major: idMajor });
-
-    if (findLevel == null) {
-      return res.json({ courses: [] })
-    }
-
-    const idLevel = findLevel._id;
-
-    const findCourse = await Course.find({ level: idLevel }, { name: 1 });
-
-    res.json({ courses: findCourse });
+    return res.json(result);
   }
 
 
