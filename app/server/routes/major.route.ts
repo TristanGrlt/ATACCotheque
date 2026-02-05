f78a5cadfd0455423b214f5fcf2b679cabbf34b1
louisgrn <louis.guerain76@gmail.com>
2026-02-05 18:21:49 +0100

    fix : Delete route

--- app/server/routes/major.route.ts
+++ app/server/routes/major.route.ts
@@ -1,9 +1,8 @@
 import { Router } from "express";
-import { getMajor } from "../controllers/major.controllers.js";
 
 const router = Router();
 
-router.get('/', getMajor);
+
 
 
 export default router;
