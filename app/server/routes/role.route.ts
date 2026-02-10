955613ad8141836982af83aff5611c83aa3e87c5
TristanGrlt <tristan.groult@gmail.com>
2026-02-10 23:25:54 +0100

    feat: add verifyPerms middleware for role-based access control in role and user routes

--- app/server/routes/role.route.ts
+++ app/server/routes/role.route.ts
@@ -2,13 +2,15 @@ import { Router } from 'express';
 import { getRole, deleteRole, createRole, updateRole } from '../controllers/role.controller.js';
 import { verify } from 'node:crypto';
 import { verifyToken } from '../middlewares/verifyToken.js';
+import { verifyPerms } from '../middlewares/verifyPerms.js';
+import { AppPermission } from '../generated/prisma/enums.js';
 
 const router = Router();
 
 router.get('/', verifyToken, getRole);
-router.post('/', verifyToken, createRole);
-router.put('/:roleId', verifyToken, updateRole);
-router.delete('/:roleId', verifyToken, deleteRole);
+router.post('/', verifyToken, verifyPerms(AppPermission.MANAGE_USERS), createRole);
+router.put('/:roleId', verifyToken, verifyPerms(AppPermission.MANAGE_USERS), updateRole);
+router.delete('/:roleId', verifyToken, verifyPerms(AppPermission.MANAGE_USERS), deleteRole);
 
 
 
--- app/server/routes/user.route.ts
+++ app/server/routes/user.route.ts
@@ -1,15 +1,17 @@
 import { Router, Request, Response } from 'express';
 import { signupUser, loginUser, logoutUser, verifyUser, getUsers, deleteUser, updateUser } from '../controllers/user.controller.js';
 import { verifyToken } from '../middlewares/verifyToken.js';
+import { verifyPerms } from '../middlewares/verifyPerms.js';
+import { AppPermission } from '../generated/prisma/enums.js';
 
 const router = Router();
 
-router.get('/', verifyToken, getUsers);
-router.delete('/:userId', verifyToken, deleteUser);
+router.get('/', verifyToken, verifyToken, verifyPerms(AppPermission.MANAGE_USERS), getUsers);
+router.delete('/:userId', verifyToken, verifyToken, verifyPerms(AppPermission.MANAGE_USERS), deleteUser);
 
-router.put('/:userId', verifyToken, updateUser);
+router.put('/:userId', verifyToken, verifyToken, verifyPerms(AppPermission.MANAGE_USERS), updateUser);
 
-router.post('/signup', verifyToken, signupUser);
+router.post('/signup', verifyToken, verifyPerms(AppPermission.MANAGE_USERS), verifyToken, signupUser);
 router.post('/login', loginUser);
 router.post('/logout', logoutUser);
 router.get('/verify', verifyUser);
