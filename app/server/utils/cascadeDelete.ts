65129bb6cddd543d220808cc1c9c5b41c4e4c8c4
TristanGrlt <tristan.groult@gmail.com>
2026-03-13 20:13:18 +0100

    feat: implement cascading delete functionality for courses and past exams, including file removal

---
+++ app/server/utils/cascadeDelete.ts
@@ -0,0 +1,51 @@
+import prisma from "../lib/prisma.js";
+import fs from "fs/promises";
+
+/*
+ * Deletes a file from the filesystem if it exists otherswise does nothing.
+ */
+async function deleteFileSafely(filePath: string | null | undefined) {
+  if (!filePath) return;
+  try {
+    await fs.unlink(filePath);
+  } catch (err: any) {
+    if (err.code !== "ENOENT") {
+    }
+  }
+}
+
+/*
+ * Deletes a past exam and all its annexes from the database and the filesystem.
+ */
+export const deletePastExamWithFiles = async (
+  pastExamId: number,
+): Promise<void> => {
+  const exam = await prisma.pastExam.findUnique({
+    where: { id: pastExamId },
+    include: { annexe: true },
+  });
+  if (!exam) return;
+
+  await Promise.all(exam?.annexe.map((a) => deleteFileSafely(a.path)));
+  await deleteFileSafely(exam.path);
+  await prisma.pastExam.delete({ where: { id: pastExamId } });
+};
+
+/*
+ * Deletes all past exams and their annexes for a given course from the filesystem.
+ */
+export async function deletePastExamForCourse(courseId: number): Promise<void> {
+  const pastExams = await prisma.pastExam.findMany({
+    where: { courseId },
+    select: { id: true },
+  });
+  await Promise.all(pastExams.map((e) => deletePastExamWithFiles(e.id)));
+}
+
+/*
+ * Deletes a course and all its past exams and annexes from the database and the filesystem.
+ */
+export async function deleteCourseCascade(courseId: number): Promise<void> {
+  await deletePastExamForCourse(courseId);
+  await prisma.course.delete({ where: { id: courseId } });
+}
