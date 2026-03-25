b29f81043ea36908546b5dbc02bb091fecfd9e2a
TristanGrlt <tristan.groult@gmail.com>
2026-03-25 12:10:20 +0100

    refactor: clean up formatting and indentation in Loading component

--- app/client/src/components/loading.tsx
+++ app/client/src/components/loading.tsx
@@ -1,30 +1,28 @@
-import { Card, CardContent, CardHeader } from "./ui/card"
-import { Spinner } from "./ui/spinner"
+import { Card, CardContent, CardHeader } from "./ui/card";
+import { Spinner } from "./ui/spinner";
 
 export const Loading = () => {
   return (
-      <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
-        <Card className="w-full max-w-sm shadow-xl">
-          <CardHeader>
-            <div className='flex flex-col items-center pt-6'>
-              <div className='bg-muted inset-0 rounded-full animate-pulse p-4 border'>
-                <Spinner className='size-8' />
-              </div>
+    <div className="bg-background flex min-h-svh flex-col items-center justify-center gap-6 p-6 md:p-10">
+      <Card className="w-full max-w-sm shadow-xl">
+        <CardHeader>
+          <div className="flex flex-col items-center pt-6">
+            <div className="bg-muted inset-0 rounded-full animate-pulse p-4 border">
+              <Spinner className="size-8" />
             </div>
-          </CardHeader>
-          <CardContent>
-            <div className='flex flex-col items-center gap-4'>
-            <h1 className='text-lg font-semibold tracking-tight text-center'>
-              Chargement en cour
+          </div>
+        </CardHeader>
+        <CardContent>
+          <div className="flex flex-col items-center gap-4">
+            <h1 className="text-lg font-semibold tracking-tight text-center">
+              Chargement en cours
             </h1>
-            <p className='text-sm text-muted-foreground'>
+            <p className="text-sm text-muted-foreground">
               Veuillez patienter quelques instants...
             </p>
-
-            </div>
-          </CardContent>
-
-        </Card>
-      </div>
-    )
-}
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  );
+};
